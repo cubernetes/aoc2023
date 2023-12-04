680222a4bdb5b63d2e1c01d3b88a8498d1e0db43
tosu <timo42@proton.me>
2023-12-04 07:36:30 +0100

    2023 3 1+2 cleanup

--- 3/solution1.py
+++ 3/solution1.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python3
 
 import re
-import sys
-from functools import lru_cache
-from collections import deque
-
 
 data = open(0).read().strip()
 lines = data.splitlines()
 
 line_len = len(lines[0])
-lines = ['.' * line_len, *lines, '.' * line_len]
+lines = ['.'*line_len, *lines, '.'*line_len]
+for i, line in enumerate(lines):
+    lines[i] = '.' + line + '.'
+
 total = 0
-for line1, line2, line3 in list(zip(lines, lines[1:], lines[2:])):
-    def one_pass(line1: str, line2: str, line3: str) -> int:
-        def inside_pass(line1: str, line2: str, match, line3: str) -> int:
-            start, end = match.span()
-            n = int(match.group(0))
-            for s in line1[max(0, start - 1) : min(line_len - 1, end + 1)]:
-                if s != '.':
-                    return n
-            for s in line3[max(0, start - 1) : min(line_len - 1, end + 1)]:
-                if s != '.':
-                    return n
-            if (start > 0 and line2[start - 1] != '.') or (end < line_len and line2[end] != '.'):
-                return n
-            return 0
-        tmp_n = 0
-        for match in re.finditer(r'\d+', line2):
-            tmp_n += inside_pass(line1, line2, match, line3)
-        return tmp_n
-    x = one_pass(line1, line2, line3)
-    total += x
+for line1, line2, line3 in zip(lines, lines[1:], lines[2:]):
+    def is_part_number(match: re.Match) -> bool:
+        start, end = match.span()
+        if line2[start-1] != '.' or line2[end] != '.':
+            return True
+        for c in line1[start-1:end+1] + line3[start-1:end+1]:
+            if c != '.': return True
+        return False
+
+    for match in re.finditer(r'\d+', line2):
+        total += int(match.group(0)) if is_part_number(match) else 0
 
 print(total)
--- 3/solution2.py
+++ 3/solution2.py
@@ -1,48 +1,32 @@
 #!/usr/bin/env python3
 
 import re
-import sys
-from functools import lru_cache
-from collections import deque
-
 
 data = open(0).read().strip()
 lines = data.splitlines()
 
 line_len = len(lines[0])
-lines = ['.' * line_len, *lines, '.' * line_len]
+lines = ['.'*line_len, *lines, '.'*line_len]
+for i, line in enumerate(lines):
+    lines[i] = '.' + line + '.'
+
 total = 0
-
-def get_neighs(line1, line2, line3, i) -> list[int]:
-    neighs = []
-    l1m = re.finditer(r'\d+', line1)
-    l2m = re.finditer(r'\d+', line2)
-    l3m = re.finditer(r'\d+', line3)
-    for m in l1m:
-        start, end = m.span()
-        n = int(m.group(0))
-        if i in list(range(start - 1, end + 1)):
-            neighs.append(n)
-            continue
-    for m in l3m:
-        start, end = m.span()
-        n = int(m.group(0))
-        if i in list(range(start - 1, end + 1)):
-            neighs.append(n)
-            continue
-    for m in l2m:
-        start, end = m.span()
-        n = int(m.group(0))
-        if i in list(range(start - 1, end + 1)):
-            neighs.append(n)
-            continue
-    return neighs
-
 for line1, line2, line3 in list(zip(lines, lines[1:], lines[2:])):
     for i, s in enumerate(line2):
-        if s == '*':
-            neighs = get_neighs(line1, line2, line3, i)
-            if len(neighs) == 2:
-                total += neighs[0] * neighs[1]
+        def get_neighs() -> list[int]:
+            neighs = []
+            line1_matches = list(re.finditer(r'\d+', line1))
+            line2_matches = list(re.finditer(r'\d+', line2))
+            line3_matches = list(re.finditer(r'\d+', line3))
+            matches = line1_matches + line2_matches + line3_matches
+            for m in matches:
+                start, end = m.span()
+                num = int(m.group(0))
+                if i in range(start - 1, end + 1):
+                    neighs.append(num)
+                    continue
+            return neighs
+        if s == '*' and len(neighs := get_neighs()) == 2:
+            total += neighs[0] * neighs[1]
 
 print(total)
