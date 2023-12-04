d7feb965d472c76facec3347789af50d41bcd6c0
tosu <timo42@proton.me>
2023-12-04 07:45:35 +0100

    2023 2 1+2 cleanup

--- 2/solution1.py
+++ 2/solution1.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
 
-import re
-import sys
-from functools import lru_cache
-from collections import deque
-
-
-data = open(0).read().strip()
-lines = data.splitlines()
-
-s = 0 
+total = 0 
 red, green, blue = 12, 13, 14
-for i, line in enumerate(lines):
-    def one_pass(line: str) -> bool:
-        line = re.sub(r'Game \d+: ', '', line)
+for i, line in enumerate(open(0)):
+    def game_possible(line: str) -> bool:
+        line = line.split(':')[1]
         sets_of_cubes = line.split('; ')
         for set_ in sets_of_cubes:
-            set_of_cubes = set_.split(', ')
-            for selection in set_of_cubes:
-                amount, color = selection.split(' ')
+            selections = set_.split(', ')
+            for selection in selections:
+                amount, color = selection.split()
                 amount = int(amount)
-                if color == 'red':
-                    if amount > red:
+                if color == 'red' and amount > red:
                         return False
-                if color == 'green':
-                    if amount > green:
+                if color == 'green' and amount > green:
                         return False
-                if color == 'blue':
-                    if amount > blue:
+                if color == 'blue' and amount > blue:
                         return False
         return True
-    if one_pass(line):
-        s += i + 1
+    if game_possible(line):
+        total += i + 1
 
-print(s)
+print(total)
--- 2/solution2.py
+++ 2/solution2.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python3
 
-import re
-import sys
-from functools import lru_cache
-from collections import deque
-
-
-data = open(0).read().strip()
-lines = data.splitlines()
-
-s = 0 
+total = 0 
 red, green, blue = 12, 13, 14
-for i, line in enumerate(lines):
-    def one_pass(line: str) -> int:
+for i, line in enumerate(open(0)):
+    def cube_set_power(line: str) -> int:
         max_red = max_green = max_blue = -1
-        line = re.sub(r'Game \d+: ', '', line)
+        line = line.split(':')[1]
         sets_of_cubes = line.split('; ')
         for set_ in sets_of_cubes:
-            set_of_cubes = set_.split(', ')
-            for selection in set_of_cubes:
-                amount, color = selection.split(' ')
+            selections = set_.split(', ')
+            for selection in selections:
+                amount, color = selection.split()
                 amount = int(amount)
-                if color == 'red':
-                    if amount > max_red:
+                if color == 'red' and amount > max_red:
                         max_red = amount
-                if color == 'green':
-                    if amount > max_green:
+                if color == 'green' and amount > max_green:
                         max_green = amount
-                if color == 'blue':
-                    if amount > max_blue:
+                if color == 'blue' and amount > max_blue:
                         max_blue = amount
-        power = max_red * max_green * max_blue
-        return power
-    power = one_pass(line)
-    s += power
+        return max_red * max_green * max_blue
+    total += cube_set_power(line)
 
-print(s)
+print(total)
