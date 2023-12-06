5574936c5d95dae62deb9b0f8bedfab61d7bf3b9
tosu <timo42@proton.me>
2023-12-06 06:56:16 +0100

    math and golfed

--- 6/solution2_math.py
+++ 6/solution2_math.py
@@ -18,9 +18,9 @@ f = lambda charging_time: charging_time * (time - charging_time)
 # charging_time * charging_time - charging_time * time + current_record_distance = 0
 
 import math
-charging_time_1 = math.ceil(time/2 - ((time/2)**2 - current_record_distance)**.5)
-charging_time_2 = math.floor(time/2 + ((time/2)**2 - current_record_distance)**.5)
+charging_time_1 = time/2 - ((time/2)**2 - current_record_distance)**.5
+charging_time_2 = time/2 + ((time/2)**2 - current_record_distance)**.5
 
 print(charging_time_1)
 print(charging_time_2)
-print(charging_time_2 - charging_time_1 + 1)
+print(math.floor(charging_time_2) - math.ceil(charging_time_1) + 1)
