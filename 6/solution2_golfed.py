969f8785b626343ebf08bfd7995d55e35259e291
tosu <timo42@proton.me>
2023-12-06 07:56:54 +0100

    more golfed

--- 6/solution2_golfed.py
+++ 6/solution2_golfed.py
@@ -1,2 +1,1 @@
-t,d=[int(''.join(l.split()[1:]))for l in open(0)]
-print(1+int(t/2+(t*t/4-d)**.5)-int(1+t/2-(t*t/4-d)**.5))
+i=int;t,d=[i(''.join(l.split()[1:]))for l in open(0)];print(i(a:=t/2+(t*t/4-d)**.5)-i(t-a))
