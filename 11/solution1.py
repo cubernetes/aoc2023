3400caaad7d7a424573ac3c4a0ecad87184a7aac
tosu <timo42@proton.me>
2023-12-11 06:40:54 +0100

    2023 11 1+2 cleanup

--- 11/solution1.py
+++ 11/solution1.py
@@ -1,46 +1,40 @@
 #!/usr/bin/env python3
 
-import re
-import sys
-import math
 import numpy as np
-from functools import lru_cache
-from collections import deque, defaultdict, Counter
-
 
 data = open(0).read().strip()
-lines__ = data.splitlines()
+lines__ = list(map(list, data.splitlines()))
 
 lines_ = []
 for line in lines__:
     if set(line) == {'.'}:
-        lines_.append(list(line))
-        lines_.append(list(line))
+        lines_.append(line)
+        lines_.append(line)
     else:
-        lines_.append(list(line))
+        lines_.append(line)
 lines_ = np.transpose(lines_)
 
 lines = []
 for line in lines_:
     if set(line) == {'.'}:
-        lines.append(list(line))
-        lines.append(list(line))
+        lines.append(line)
+        lines.append(line)
     else:
-        lines.append(list(line))
+        lines.append(line)
 lines = np.transpose(lines)
 
-positions = []
+galaxies = []
 for r, line in enumerate(lines):
     for c, char in enumerate(line):
         if char == '#':
-            positions.append((r, c))
+            galaxies.append((r, c))
 
 distances = 0
-for pos in positions:
-    for pos_ in positions:
-        if pos != pos_:
-            r_diff = abs(pos[0] - pos_[0])
-            c_diff = abs(pos[1] - pos_[1])
+for galaxy_a in galaxies:
+    for galaxy_b in galaxies:
+        if galaxy_a != galaxy_b:
+            r_diff = abs(galaxy_a[0] - galaxy_b[0])
+            c_diff = abs(galaxy_a[1] - galaxy_b[1])
             distances += r_diff + c_diff
 
 print(distances / 2)
--- 11/solution2.py
+++ 11/solution2.py
@@ -4,13 +4,13 @@ import numpy as np
 
 data = open(0).read().strip()
 lines = list(map(list, data.splitlines()))
+lines_t = np.transpose(lines)
 
-positions = []
+galaxies = []
 for r, line in enumerate(lines):
     for c, char in enumerate(line):
         if char == '#':
-            positions.append((r, c))
-lines_t = np.transpose(lines)
+            galaxies.append((r, c))
 
 empties = []
 empties_t = []
@@ -21,23 +21,24 @@ for c, line_t in enumerate(lines_t):
     if set(line_t) == {'.'}:
         empties_t.append(c)
 
+factor = 1_000_000
 distances = 0
-for pos in positions:
-    for pos_ in positions:
-        if pos != pos_:
-            r_diff = abs(pos[0] - pos_[0])
-            c_diff = abs(pos[1] - pos_[1])
-            p = min(pos[0], pos_[0])
-            for r in range(p + 1, p + r_diff):
+for galaxy_a in galaxies:
+    for galaxy_b in galaxies:
+        if galaxy_a != galaxy_b:
+            r_diff = abs(galaxy_a[0] - galaxy_b[0])
+            c_diff = abs(galaxy_a[1] - galaxy_b[1])
+
+            r_start = min(galaxy_a[0], galaxy_b[0])
+            for r in range(r_start + 1, r_start + r_diff):
                 if r in empties:
-                    r_diff += 1_000_000 - 1
-            p = min(pos[1], pos_[1])
-            for c in range(p + 1, p + c_diff):
+                    r_diff += factor - 1
+
+            c_start = min(galaxy_a[1], galaxy_b[1])
+            for c in range(c_start + 1, c_start + c_diff):
                 if c in empties_t:
-                    c_diff += 1_000_000 - 1
+                    c_diff += factor - 1
+
             distances += r_diff + c_diff
 
 print(distances / 2)
-
-# wrong: 82000210
-
