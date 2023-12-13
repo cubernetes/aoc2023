64776fa449cc4786e690fd31c49e20b0a0c32762
tosu <timo42@proton.me>
2023-12-13 13:00:17 +0100

    2023 13 1+2

---
+++ 13/solution2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+patterns = open(0).read().split('\n\n')
+
+t = 0
+for i, pattern in enumerate(patterns):
+    pattern = np.array([list(line) for line in pattern.splitlines()])
+    for row in range(len(pattern) - 1):
+        up = row
+        down = row + 1
+        j = 0
+        err = 0
+        while up - j >= 0 and down + j < len(pattern):
+            for a, b in zip(pattern[up - j], pattern[down + j]):
+                if a != b:
+                    err += 1
+            j += 1
+        if err == 1:
+            t += (row + 1) * 100
+    pattern = np.transpose(pattern)
+    for column in range(len(pattern) - 1):
+        up = column
+        down = column + 1
+        j = 0
+        err = 0
+        while up - j >= 0 and down + j < len(pattern):
+            for a, b in zip(pattern[up - j], pattern[down + j]):
+                if a != b:
+                    err += 1
+            j += 1
+        if err == 1:
+            t += column + 1
+
+print(t)
